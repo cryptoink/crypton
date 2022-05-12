66628aaeb5fcf3e051e971837991c9de965675ba
feliks <50949993+cryptoink@users.noreply.github.com>
2022-05-12 11:52:15 +0300

    Update Currency.cpp

--- src/CryptoNoteCore/Currency.cpp
+++ src/CryptoNoteCore/Currency.cpp
@@ -100,11 +100,11 @@ bool Currency::getBlockReward(size_t medianSize, size_t currentBlockSize, uint64
   uint64_t fee, uint64_t& reward, int64_t& emissionChange) const {
   assert(alreadyGeneratedCoins <= m_moneySupply);
   assert(m_emissionSpeedFactor > 0 && m_emissionSpeedFactor <= 8 * sizeof(uint64_t));
-  uint64_t fspeedfactorw = m_moneySupply / 2;
+ // uint64_t fspeedfactorw = m_moneySupply / 4;
  uint64_t m_emissionSpeedFactorw = m_emissionSpeedFactor;
-if (alreadyGeneratedCoins < fspeedfactorw){m_emissionSpeedFactorw = 1;}
-
-    uint64_t baseReward = (m_moneySupply - alreadyGeneratedCoins) >> m_emissionSpeedFactorw; 
+if (alreadyGeneratedCoins < fspeedfactorw) {m_emissionSpeedFactorw = 999999999999; }
+else {
+  uint64_t baseReward = (m_moneySupply - alreadyGeneratedCoins) >> m_emissionSpeedFactorw; }
 
   medianSize = std::max(medianSize, m_blockGrantedFullRewardZone);
   if (currentBlockSize > UINT64_C(2) * medianSize) {
